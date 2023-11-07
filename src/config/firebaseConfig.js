746a2d6b2e87695b955d202a7a56e2c528572e30
PAWAN KUMAR SAHU <89339624+coderpawan@users.noreply.github.com>
2023-11-07 16:11:33 +0530

    Updated

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyDsAHMZFPyiDkVa5DFKC9_BmdAYlv-30Xk",
+  authDomain: "cryptonova-826f3.firebaseapp.com",
+  projectId: "cryptonova-826f3",
+  storageBucket: "cryptonova-826f3.appspot.com",
+  messagingSenderId: "737532841719",
+  appId: "1:737532841719:web:1e31dc9ea57e6b6b820df2",
+  measurementId: "G-CH2JEJGFWY",
+};
+
+export default firebaseConfig;
